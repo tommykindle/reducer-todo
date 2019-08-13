3f7c21e92222ad603000e229057bcbb60e04c0e0
Tommy <tommykindle83@gmail.com>
2019-08-13 12:05:43 -0700

    Project set up reducer file started and intial state set up

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,15 @@
+export const initialState = {
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+}
+
+
+export const todoReducer = (state, action) => {
+
+
+
+
+  defautl:
+  return state;
+}

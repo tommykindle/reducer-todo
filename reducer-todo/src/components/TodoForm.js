403e774af669c79c94f10d7f357b30cf331c35e4
Tommy <tommykindle83@gmail.com>
2019-08-13 13:02:42 -0700

    Ready to test basic functionality of adding a todo

--- reducer-todo/src/components/TodoForm.js
+++ reducer-todo/src/components/TodoForm.js
@@ -7,18 +7,34 @@ const Todo = () => {
   const [newTodo, setNewTodo] = useState();
 
   const [state, dispatch] = useReducer(todoReducer, initialsState);
-}
 
-return (
-  <div>
-    <h1>Todo List</h1>
-    <input
-      className="todo-input"
-      type="text"
-      name="newTodo"
-      value={newTodo}
-      onChange={handleChanges}
-    />
+  const handleChanges = e => {
+    setNewTodo(e.target.value);
+  }
 
-  </div>
-)
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        <li>{state.item}{' '}</li>
+      </ul>
+      <input
+        className="todo-input"
+        type="text"
+        name="newTodo"
+        value={newTodo}
+        onChange={handleChanges}
+      />
+      <button
+        onClick={() =>
+          dispatch({ type: 'ADD_TODO', payload: newTodo })
+        }
+      >
+        Add Todo
+</button>
+    </div>
+  );
+
+};
+
+export default Todo;
